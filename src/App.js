4a0baab848722d32c8cad0dc6dbad32f4b0f7a18
anandbaraik <anandbaraik2014@gmail.com>
2022-06-02 23:23:47 +0530

    update:refactored & dark/light mode fature added :zap:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-function App() {
+import React, { useState } from 'react'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Routes from "./components/Routes";
+const App = () => {
+    let isGscDark =  localStorage.getItem("isGscDark") || "false";
+    const [darkTheme, setDarkTheme] = useState(isGscDark === "true");
+    const setThemeDark = () => {
+        let isDark = !darkTheme;
+        localStorage.setItem('isGscDark', isDark);
+        setDarkTheme(isDark);
+    }
   return (
-    <h1 className="text-3xl underline decoration-red-500 font-bold">
-      Hello world!
-    </h1>
-  );
+    <div className={darkTheme ? 'dark' : ''}>
+        <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+            <Navbar
+                darkTheme={darkTheme}
+                setThemeDark={setThemeDark}
+            />
+            <Routes/>
+            <Footer/>
+        </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './Global.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import "./global.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<Router><App/></Router>, document.getElementById("root"));
