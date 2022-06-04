09c119e0f79c8f809d88e4b62cc0d7904b6230c3
anandbaraik <anandbaraik2014@gmail.com>
2022-06-04 12:19:57 +0530

    update:footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200'>
+    	<h1>
+		ⓒ 2021 Google Search , Inc.
+      	</h1>
+    </div>
   )
 }
 
