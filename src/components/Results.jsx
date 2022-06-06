7d8007b90752dbbc02df060f75e0487688b97c32
anandbaraik <anandbaraik2014@gmail.com>
2022-06-06 22:25:57 +0530

    update:video link check added

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -73,12 +73,17 @@ export const Results = () => {
 					{
 						results.map((video, index) => (
 							<div key={index} className="p-2">
-								<ReactPlayer
-									url={video?.additional_links?.[0].href}
-									controls
-									width="355px"
-									height="200px"
-								/>
+								{
+									video?.additional_links?.[0].href &&
+									(
+										<ReactPlayer
+											url={video?.additional_links?.[0].href}
+											controls
+											width="355px"
+											height="200px"
+										/>
+									)
+								}
 							</div>
 						))
 					}
